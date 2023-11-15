e814be75f065186c6fb551e0402324c577aac95f
Leonardo <leobonadia.dev@gmail.com>
2023-11-15 16:30:01 -0300

    Correções do autoGenDoc.js

--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -4,32 +4,45 @@ const swaggerAutogen = require('swagger-autogen')({
     language: 'pt-BR'
 });
 
-const outputFile = './swagger_output.json';
-const endpointsFiles = ['../index.js', '../src/routes.js']
+let outputFile = './swagger_output.json';
+let endpointsFiles = ['../index.js', '../src/routes.js'];
+
+if(String(process.env.OS).toLocaleLowerCase().includes("windows")){
+    outputFile = './swagger/swagger_output.json';
+    endpointsFiles = ['./index.js', './src/routes.js'];
+}
+
 
 let doc = {
     info: {
         version: "1.0.0",
         title: "API do BoardTasks",
-        description: "Documentação da API do BoardTasks"
+        description: "Documentação da API do BoardTasks."
     },
     servers: [
         {
-            url: 'http://localhost:4000',
-            description: 'Servidor do localhost.'
+            url: "http://localhost:4000/",
+            description: "Servidor localhost."
         },
         {
-            url: 'https://boardtasks-back.vercel.app',
-            description: 'Servidor de produção.'
+            url: "https://dnc-board-tasks-back.vercel.app/",
+            description: "Servidor de produção."
         }
     ],
     consumes: ['application/json'],
     produces: ['application/json'],
+    components: {
+        schemas: {
+            Usuario: mongooseToSwagger(EsquemaUsuario),
+            Tarefa: mongooseToSwagger(EsquemaTarefa)
+        }
+    }
 }
 
+
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    console.log("Documentação do Swager gerada encontra-se no arquivo em: "+ outputFile);
-    if(process.env.NODE_ENV !== 'production'){
-        require('../index.js')
+    console.log("Documentação do Swagger gerada encontra-se no arquivo em: " + outputFile);
+    if (process.env.NODE_ENV !== 'production') {
+        require("../index.js");
     }
 })
