dd33d21164c6e79c2c3a27633935862d76c8233a
Leonardo <leobonadia.dev@gmail.com>
2023-11-15 16:57:24 -0300

    Quase desistindo,  mais correções pra ver se roda em produção

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 function routes(app) {
-    app.use('/users', require('./routes/users.js'));
-    return
+    app.use('/usuario', require('./routes/usuario.js'));
+    app.use('/tarefa', require('./routes/tarefa.js'));
+    return;
 }
 
 module.exports = routes;
